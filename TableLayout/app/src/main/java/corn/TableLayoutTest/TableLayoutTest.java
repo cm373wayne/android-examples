5b66603b1734fdef8730492cc39a21a84b4ae81e
Wayne Chang <wayne@cmoney.net.tw>
2015-07-15 13:51:43 +0800

    隨意拉出的TableLayout

---
+++ TableLayout/app/src/main/java/corn/TableLayoutTest/TableLayoutTest.java
@@ -0,0 +1,15 @@
+package corn.TableLayoutTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TableLayoutTest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+    }
+}
